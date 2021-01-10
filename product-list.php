223bf616e24eedf45a84884f1c5765e36e97c922
Yusufhan Kırçova <59140093+kircova@users.noreply.github.com>
2021-01-10 16:06:46 +0300

    Product-List Filter Bug Fix

--- product-list.php
+++ product-list.php
@@ -24,7 +24,7 @@ session_start();
   else if(isset($_GET["product_search"]))
   {
     $query = $_GET["product_search"];
-    $sql_statement = "SELECT *
+    $sql_statement = "SELECT product.prid, product.pname, product.artist, product.genre, product.description, product.price, product.productImgUrl, product.IsVisible, product.categoryId
                             FROM product, songs
                             WHERE isVisible=1 AND
                           (product.pname LIKE '%$query%') OR (product.artist LIKE '%$query%')  OR (product.description LIKE '%$query%')  OR ( songs.songname LIKE '%$query%') AND product.prid = songs.prid
