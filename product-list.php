e59b5b1293a4c405c8ed361b620d32bda2028ce8
battlemanyako <58150776+battlemanyako@users.noreply.github.com>
2021-01-11 15:05:12 +0300

    Sidebar updated.

--- product-list.php
+++ product-list.php
@@ -121,7 +121,7 @@ session_start();
 <?php
   $sql_statement = "SELECT genre
                               FROM `product`
-                              WHERE isVisible=1 
+                              WHERE isVisible=1
                               GROUP BY genre";
   $search_result = mysqli_query($db, $sql_statement);
 
@@ -204,19 +204,6 @@ session_start();
                               </nav>
                           </div>
 
-
-
-                      <div class="sidebar-widget brands">
-                          <h2 class="title">Our Brands</h2>
-                          <ul>
-                              <li><a href="#">Nulla </a><span>(45)</span></li>
-                              <li><a href="#">Curabitur </a><span>(34)</span></li>
-                              <li><a href="#">Nunc </a><span>(67)</span></li>
-                              <li><a href="#">Ullamcorper</a><span>(74)</span></li>
-                              <li><a href="#">Fusce </a><span>(89)</span></li>
-                              <li><a href="#">Sagittis</a><span>(28)</span></li>
-                          </ul>
-                      </div>
                   </div>
                   <!-- Side Bar End -->
                     <div class="col-lg-8">
